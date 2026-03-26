db1673b1aa609f83b1b4f6b635771cc54f0e1c89
Tim <code@tim.life>
2026-03-25 20:32:08 -0700

    refactor: redirect root editable install from src/ to packages/undef-terminal/src/
    Update root pyproject.toml packages.find.where to point at packages/undef-terminal/src
    instead of the stale src/ tree. Delete src/undef/ (all files were older than their
    packages/ counterparts). Also extend codespell skip to cover vendored structlog files.

--- src/undef/terminal/server/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-# SPDX-FileCopyrightText: Copyright (c) 2025-2026 MindTenet LLC. All rights reserved.
-# SPDX-License-Identifier: AGPL-3.0-or-later
-#
-
-"""Route modules for the hosted terminal server."""
